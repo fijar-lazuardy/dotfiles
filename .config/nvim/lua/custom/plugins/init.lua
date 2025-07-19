f95adcabcacbbba69940b6931e5364359b951130
Fijar Lazuardy <fijarlaz@gmail.com>
2025-07-19 21:28:48 +0700

    new config

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -10,7 +10,6 @@ return {
 	"tpope/vim-rhubarb",
 
 	-- Detect tabstop and shiftwidth automatically
-	"tpope/vim-sleuth",
 
 	-- NOTE: This is where your plugins related to LSP can be installed.
 	--  The configuration is done below. Search for lspconfig to find it below.
--- .config/nvim/lua/custom/plugins/noice.lua
+++ .config/nvim/lua/custom/plugins/noice.lua
@@ -1,21 +1,29 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-    messages = {
-    -- NOTE: If you enable messages, then the cmdline is enabled automatically.
-    -- This is a current Neovim limitation.
-    enabled = false, -- enables the Noice messages UI
-    view = "notify", -- default view for messages
-    view_error = false, -- view for errors
-    view_warn = "notify", -- view for warnings
-    view_history = "messages", -- view for :messages
-    view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
-  },
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    }
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		lsp = {
+			hover = {
+				enabled = false, -- 👈 turn off Noice hover so native one with border works
+			},
+			signature = {
+				enabled = false,
+			},
+		},
+		-- add any options here
+		messages = {
+			-- NOTE: If you enable messages, then the cmdline is enabled automatically.
+			-- This is a current Neovim limitation.
+			enabled = false, -- enables the Noice messages UI
+			view = "notify", -- default view for messages
+			view_error = false, -- view for errors
+			view_warn = "notify", -- view for warnings
+			view_history = "messages", -- view for :messages
+			view_search = "virtualtext", -- view for search count messages. Set to `false` to disable
+		},
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+	},
 }
